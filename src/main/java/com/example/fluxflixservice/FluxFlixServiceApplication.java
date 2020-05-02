054d9c6c54ffeee4a06ff645d805eed86a9405ae
Rafael <elgondu@gmail.com>
2020-05-01 23:39:05 -0400

    se agregan productos de prueba

--- src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
+++ src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
@@ -1,13 +1,37 @@
 package com.example.fluxflixservice;
 
+import com.example.fluxflixservice.models.dao.ProductoDao;
+import com.example.fluxflixservice.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class FluxFlixServiceApplication {
+public class FluxFlixServiceApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(FluxFlixServiceApplication.class);
+    private ProductoDao dao;
+
+    public FluxFlixServiceApplication(ProductoDao dao) {
+        this.dao = dao;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FluxFlixServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flux.just(
+                new Producto("playstation 5", 200.000),
+                new Producto("lenovo legion", 320.980),
+                new Producto("dell xps 15", 543.980),
+                new Producto("tv sansumg", 150.000)
+        )
+                .flatMap(producto -> dao.save(producto))
+                .subscribe(producto -> log.info("Insert:" + producto.getId() + "" + producto.getNombre()));
+    }
 }
--- src/main/java/com/example/fluxflixservice/models/documents/Producto.java
+++ src/main/java/com/example/fluxflixservice/models/documents/Producto.java
@@ -6,17 +6,35 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
 @Document(collection = "productos")
-public class documents {
+public class Producto {
+
+    public Producto(){}
 
     @Id
     private String id;
+    private String nombre;
     private Double precio;
     private Date createAt;
+
+    public Producto(String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Producto setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
     public String getId() {
         return id;
     }
 
-    public documents setId(String id) {
+    public Producto setId(String id) {
         this.id = id;
         return this;
     }
@@ -25,7 +43,7 @@ public class documents {
         return precio;
     }
 
-    public documents setPrecio(Double precio) {
+    public Producto setPrecio(Double precio) {
         this.precio = precio;
         return this;
     }
@@ -34,7 +52,7 @@ public class documents {
         return createAt;
     }
 
-    public documents setCreateAt(Date createAt) {
+    public Producto setCreateAt(Date createAt) {
         this.createAt = createAt;
         return this;
     }
