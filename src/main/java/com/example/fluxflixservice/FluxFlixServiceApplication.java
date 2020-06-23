f57db93e3708933825060fdb3b9db46d136ba531
Rafael <elgondu@gmail.com>
2020-06-22 22:52:57 -0400

    add products test

--- src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
+++ src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
@@ -37,6 +37,10 @@ public class FluxFlixServiceApplication implements CommandLineRunner {
                 new Producto("playstation 5", 200.000),
                 new Producto("lenovo legion", 320.980),
                 new Producto("dell xps 15", 543.980),
+                new Producto("tv sansumg", 150.000),
+                new Producto("playstation 6", 200.000),
+                new Producto("lenovo legion", 320.980),
+                new Producto("dell xps 15", 543.980),
                 new Producto("tv sansumg", 150.000)
         )
                 .flatMap(producto -> {
